fce16d31ea33ff29d02b2c3f51451502db0e3838
Yu Jun Zhao <yuj.zhao4@gmail.com>
2019-07-22 21:08:24 -0700

    bundled up

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,27 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./server.ts"
+  mode: "development",
+  entry: "./server.js",
+  target: "node",
+  externals: [nodeExternals()],
+  // module: {
+  //   rules: [
+  //     {
+  //       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+  //       test: /\.tsx?$/,
+  //       use: "ts-loader",
+  //       exclude: /node_modules/
+  //     }
+  //   ]
+  // },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
